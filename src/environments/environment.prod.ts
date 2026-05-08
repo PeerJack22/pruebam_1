070a7503f481627ea0abbfa8e2ce1c60948c96f4
Paulo Cisneros <pajocina.11@gmail.com>
2026-05-08 17:04:06 -0500

    Delete src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://bggqnajzjeunbtslnsjv.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnZ3FuYWp6amV1bmJ0c2xuc2p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzQzMTQsImV4cCI6MjA5Mzc1MDMxNH0.F6Kh-IDQ3iecLjysFx86E-mBaNBxk35cemkNtZQH9jw',
-};
